3c596521a9bc8bca068c19cdd809e7ec737d538a
Ryan Pablo <ryanprogramacaoedev@gmail.com>
2025-10-02 20:32:18 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿double m, cm, km;
+Console.Write("Entre com a medida (em metros): ");
+m = Convert.ToDouble(Console.ReadLine());
+cm = m * 100;
+km = m / 1000;
+Console.WriteLine("--- Equivalência ---");
+Console.WriteLine($"{cm} cm");
+Console.WriteLine($"{m} m");
+Console.WriteLine($"{km} km");
